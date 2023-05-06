bb41be1bc64f3c9270507eede198daed21f6f24b
Krunal Tule <Krunal.Tule@connectwise.com>
2023-05-06 15:22:26 +0530

    resolved eslint warnings

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
-import { useEffect, useRef } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Shimmer from './components/Shimmer';
+
 import ListView from './components/ListView';
 
 function App() {
